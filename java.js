3ba76e235b57dd8a5e39b84939986ed963a21f18
benjaaaap <162449510+benjaaaap@users.noreply.github.com>
2024-03-26 11:05:54 -0300

    Add files via upload

---
+++ java.js
@@ -0,0 +1,35 @@
+// declarando variables string 
+
+let Primernombre = "alejandro"
+let segundonombre = "antonio"
+let apellido = "viera"
+let segundoapellido = "rojas"
+
+console.log (nombre)
+
+// declarando integers
+
+let edad = 16
+let direccion = 10050
+var estatura = 1.73
+var año = 2024
+
+// declarando boleanos
+
+let registrado = false
+let humano = true
+let humanoide = false
+let logeado = true 
+
+// declarando arreglos
+
+let arreglo = ["alejandro", "3°A", "viera", 16,true]
+let arregloVacio = []
+let arregloConValores = [nombre, apellido]
+let arreglito = [0,22, [1,3]]
+
+// declarando objetos
+let persona = {curso: "3°A", generacion: 2024}
+let alumno = {nombre: "alejandro", apellido: "viera"}
+let objetoVacio = {}
+let alumne = {nombre:Primernombre, apellido: Primerapellido, edad:edad}
